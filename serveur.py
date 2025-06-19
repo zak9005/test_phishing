73dbfa81098c7f23f8e4c5319d6aa0587d33ae9b
zak9005 <znoui56@gmail.com>
2025-06-20 00:42:04 +0200

    Fix indentation and dynamic port

--- serveur.py
+++ serveur.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -17,5 +18,7 @@ def login():
     return "<h1>Connexion en cours...</h1>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
