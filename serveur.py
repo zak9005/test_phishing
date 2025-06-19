fc9a039877be155b79d214bc57dd4aeb5b981c88
zak9005 <znoui56@gmail.com>
2025-06-20 01:03:03 +0200

    Corrige le port pour Render

--- serveur.py
+++ serveur.py
@@ -18,7 +18,7 @@ def login():
     return "<h1>Connexion en cours...</h1>"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
 
