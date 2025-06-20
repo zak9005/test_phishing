bc9f44496c71044808913c599548f82dc055d797
zak9005 <znoui56@gmail.com>
2025-06-20 03:06:36 +0200

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -18,7 +18,7 @@ def login():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
